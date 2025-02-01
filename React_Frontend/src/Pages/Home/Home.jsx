85333ec1f44117d616639202c3b8fa2a61881a3a
luisrodbert <luisguezsosa@gmail.com>
2025-02-01 23:15:04 +0000

    styling

--- React_Frontend/src/Pages/Home/Home.jsx
+++ React_Frontend/src/Pages/Home/Home.jsx
@@ -116,56 +116,78 @@ function Home() {
                 </Select>
               </FormControl>
 
-              {/* DEPARTURE DATE (Placeholder con "Departure Date") */}
               <FormControl
-                variant="outlined"
-                sx={{ minWidth: 190, bgcolor: 'white', borderRadius: 2 }}
+                variant="standard"
+                sx={{
+                  minWidth: 190,
+                  bgcolor: 'white',
+                  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                }}
               >
                 <TextField
                   id="Ida"
-                  type="date"
+                  type={selectedDepartureDate ? 'date' : 'text'}
+                  value={selectedDepartureDate || ''}
+                  onFocus={(e) => (e.target.type = 'date')}
+                  onBlur={(e) => !e.target.value && (e.target.type = 'text')}
                   placeholder="Departure Date"
                   InputLabelProps={{ shrink: true }}
                   inputProps={{
-                    style: { color: '#003366', fontWeight: 'bold' },
+                    style: {
+                      color: '#003366',
+                      fontWeight: 900,
+                      textAlign: 'center',
+                    },
                   }}
                   sx={{
                     bgcolor: 'white',
-                    borderRadius: 2,
                     '& input::placeholder': {
                       color: '#003366',
-                      fontWeight: 'bold',
+                      fontWeight: 900,
+                      opacity: 1,
                     },
                   }}
                   onChange={handleDateDepartureChange}
                 />
               </FormControl>
 
-              {/* RETURN DATE (Placeholder con "Return Date") */}
               <FormControl
-                variant="outlined"
-                sx={{ minWidth: 190, bgcolor: 'white', borderRadius: 2 }}
+                variant="standard"
+                sx={{
+                  minWidth: 190,
+                  bgcolor: 'transparent',
+
+                  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                }}
               >
                 <TextField
                   id="Vuelta"
-                  type="date"
+                  type={selectedReturnDate ? 'date' : 'text'}
+                  value={selectedReturnDate || ''}
+                  onFocus={(e) => (e.target.type = 'date')}
+                  onBlur={(e) => !e.target.value && (e.target.type = 'text')}
                   placeholder="Return Date"
                   InputLabelProps={{ shrink: true }}
                   inputProps={{
-                    style: { color: '#003366', fontWeight: 'bold' },
+                    style: {
+                      color: '#003366',
+                      fontWeight: 'bold',
+                      textAlign: 'center',
+                    },
                   }}
                   sx={{
                     bgcolor: 'white',
-                    borderRadius: 2,
+
                     '& input::placeholder': {
                       color: '#003366',
                       fontWeight: 'bold',
+                      opacity: 1,
+                      border: 'none',
                     },
                   }}
                   onChange={handleDateReturnChange}
                 />
               </FormControl>
-
               {/* PASSENGERS (igual que todos los demás) */}
               <FormControl
                 variant="outlined"
@@ -188,150 +210,69 @@ function Home() {
           </section>
         </Box>
         <section id="Content">
-          <div id=" Oferta1" className="Oferta">
-            <Card sx={{ maxWidth: 345, marginTop: '20px' }}>
+          {[
+            {
+              title: 'Madrid',
+              image:
+                'https://www.spain.info/.content/imagenes/cabeceras-grandes/madrid/calle-gran-via-madrid-s333961043.jpg',
+              description:
+                'Madrid is a cosmopolitan city, faithful to its customs, culture, and its art, but at the same time, it is a modern place. It houses first-class cultural, economic, and political centers in Europe.',
+            },
+            {
+              title: 'París',
+              image:
+                'https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2022/10/20/torre-eiffel-y-el-rio-sena-en-paris.jpeg',
+              description:
+                'Explore Paris, the charming City of Light. Enjoy the Eiffel Tower, the Louvre, romantic walks along the Seine, and delicious French cuisine. Book your flight and live an unforgettable experience in the capital of France.',
+            },
+            {
+              title: 'Barcelona',
+              image:
+                'https://www.berlinsbi.com/uploads/sites/2/2023/06/web-banner-barcelona.jpg?w=1854&h=1043&crop=1',
+              description:
+                "Discover Barcelona, a vibrant Mediterranean city. Admire Gaudí's architecture, relax on its beaches, enjoy delicious Catalan food, and explore Las Ramblas. Reserve your Fly and experience the magic of Barcelona.",
+            },
+            {
+              title: 'Sevilla',
+              image:
+                'https://eu-central-1.linodeobjects.com/tecnohotelnews/2022/12/shutterstock_649614043.jpg',
+              description:
+                'Discover Seville, heart of Andalusia. Marvel at the Giralda, the majestic Cathedral, and the Alcázar. Enjoy tapas, flamenco, and walks through the Santa Cruz neighborhood. Book your flight and experience its unique charm.',
+            },
+            {
+              title: 'Lanzarote',
+              image: 'img/lanzarote.jpg',
+              description:
+                'Explore Lanzarote, a volcanic island in the Canary Islands. Enjoy your beaches, unique landscapes of Timanfaya, and wine cellars. Relax at Jameos del Agua and César Manrique. Book your flight and live an unforgettable experience.',
+            },
+            {
+              title: 'Cádiz',
+              image:
+                'https://tourscanner.com/blog/wp-content/uploads/2023/11/que-ver-y-hacer-en-Cadiz.jpeg',
+              description:
+                'Discover Cádiz, an Andalusian coastal city. Relax in your golden beaches, explore the old town, and taste fresh fish. Enjoy the carnival and the rich history of Cádiz. Book your flight and experience Cádiz to the fullest.',
+            },
+          ].map((destination, index) => (
+            <Card key={index} className="Oferta">
               <CardMedia
-                sx={{ height: 160 }}
-                image="https://www.spain.info/.content/imagenes/cabeceras-grandes/madrid/calle-gran-via-madrid-s333961043.jpg"
-                title="Madrid"
+                component="img"
+                image={destination.image}
+                alt={destination.title}
               />
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Madrid
+                <Typography className="card-title">
+                  {destination.title}
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Madrid is a cosmopolitan city, faithful to its customs,
-                  culture and its art, but at the same time it is a modern
-                  place, It houses first-class cultural, economic and political
-                  centers order in Europe
+                <Typography className="card-description">
+                  {destination.description}
                 </Typography>
               </CardContent>
-              <CardActions>
-                <Button size="small">Reserve</Button>
-                <span size="small">Learn More</span>
+              <CardActions className="card-buttons">
+                <Button className="card-button">Reserve</Button>
+                <Button className="card-button learn-more">Learn More</Button>
               </CardActions>
             </Card>
-          </div>
-          <div id=" Oferta2 " className="Oferta">
-            <Card sx={{ maxWidth: 345, marginTop: '20px' }}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image="https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2022/10/20/torre-eiffel-y-el-rio-sena-en-paris.jpeg"
-                title="París"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  París
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Explore Paris, the charming City of Light. Enjoy the Eiffel
-                  Tower, the Louvre, romantic walks along the Seine and
-                  delicious French cuisine. Book your flight and live an
-                  unforgettable experience in the capital of France.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Reserve</Button>
-                <span size="small">Learn More</span>
-              </CardActions>
-            </Card>
-          </div>
-          <div id=" Oferta3 " className="Oferta">
-            <Card sx={{ maxWidth: 345, marginTop: '20px' }}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image="https://www.berlinsbi.com/uploads/sites/2/2023/06/web-banner-barcelona.jpg?w=1854&h=1043&crop=1"
-                title="Barcelona"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Barcelona
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Discover Barcelona, ​​a vibrant Mediterranean city. Admire
-                  Gaudí's architecture, relax on its beaches, enjoy delicious
-                  Catalan food and explore Las Ramblas. Reserve your Fly and
-                  experience the magic of Barcelona.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Reserve</Button>
-                <span size="small">Learn More</span>
-              </CardActions>
-            </Card>
-          </div>
-          <div id=" Oferta4 " className="Oferta">
-            <Card sx={{ maxWidth: 345, marginTop: '20px' }}>
-              <CardMedia
-                sx={{ height: 160 }}
-                image="https://eu-central-1.linodeobjects.com/tecnohotelnews/2022/12/shutterstock_649614043.jpg"
-                title="Sevilla"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Sevilla
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Discover Seville, heart of Andalusia. Marvel at the Giralda,
-                  the majestic Cathedral and the Alcázar. Enjoy tapas, flamenco
-                  and walks through the Santa Cruz neighborhood. Booking your
-                  flight and experience its unique charm.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Reserve</Button>
-                <span size="small">Learn More</span>
-              </CardActions>
-            </Card>
-          </div>
-          <div id=" Oferta5 " className="Oferta">
-            <Card sx={{ maxWidth: 345, marginTop: '20px' }}>
-              <CardMedia
-                sx={{ height: 140 }}
-                image="img/lanzarote.jpg"
-                title="Lanzarote"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lanzarote
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Explore Lanzarote, a volcanic island in the Canary Islands.
-                  Enjoy your beaches, unique landscapes of Timanfaya, and wine
-                  cellars. Relax at Jameos del Agua and César Manrique. Book
-                  your flight and live an unforgettable experience.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Reserve</Button>
-                <span size="small">Learn More</span>
-              </CardActions>
-            </Card>
-          </div>
-          <div id=" Oferta6 " className="Oferta">
-            <Card sx={{ maxWidth: 345, marginTop: '20px' }}>
-              <CardMedia
-                sx={{ height: 160 }}
-                image="https://tourscanner.com/blog/wp-content/uploads/2023/11/que-ver-y-hacer-en-Cadiz.jpeg"
-                title="Cádiz"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Cadiz
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Discover Cádiz, an Andalusian coastal city. Relax in your
-                  golden beaches, explore the old town, and taste fish fresh.
-                  Enjoy the carnival and the rich history of Cádiz. Book your
-                  flight and experience Cádiz to the fullest.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Reserve</Button>
-                <span size="small">Learn More</span>
-              </CardActions>
-            </Card>
-          </div>
+          ))}
         </section>
         <section id="Promociones">
           <div id="infoPromo">
